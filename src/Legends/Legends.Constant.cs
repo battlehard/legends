a77a5f7c399a2c83f0cb5da6e1d1e59cbc6040d6
Rawich Kiriyapong <rawich_kiriyapong@hotmail.com>
2023-09-17 09:53:40 +0700

    Add ListNftPool with Pagination method

--- src/Legends/Legends.Constant.cs
+++ src/Legends/Legends.Constant.cs
@@ -3,5 +3,7 @@
   public partial class Legends
   {
     private const string CONTRACT_NAME = "Legends";
+    private const int MAX_PAGE_LIMIT = 100;
+
   }
 }
--- test/LegendsTradeTests.cs
+++ test/LegendsTradeTests.cs
@@ -9,6 +9,7 @@ using Neo.Persistence;
 using Neo.SmartContract;
 using Neo.VM;
 using NeoTestHarness;
+using System.Numerics;
 using TestSwappables; // For reference contract method interface e.g. Legends
 using Xunit;
 
@@ -106,6 +107,32 @@ namespace test
       engine.ExecuteScript<Legends>(c => c.trade(Common.LEGENDS_ADMIN, Common.LEGENDS_OWNER));
       engine.State.Should().Be(VMState.FAULT);
       engine.UncaughtException.GetString().Should().Contain("LegendsOwner in not available in the pool");
+    }
+
+    [Fact]
+    public void List_page_with_failed_cases()
+    {
+      ProtocolSettings settings = chain.GetProtocolSettings();
+      UInt160 owner = chain.GetDefaultAccount("owner").ToScriptHash(settings.AddressVersion);
+
+      using SnapshotCache snapshot = fixture.GetSnapshot();
+      TestApplicationEngine engineStep1 = new(snapshot, settings, owner, WitnessScope.Global);
+      BigInteger nftQuantity = Common.MAX_PAGE_LIMIT + 1;
+
+      using TestApplicationEngine engineStep2 = new(snapshot, settings, owner, WitnessScope.Global);
+      engineStep2.ExecuteScript<Legends>(c => c.listNftPool(1, nftQuantity)); // List more than max page limit.
+      engineStep2.State.Should().Be(VMState.FAULT);
+      engineStep2.UncaughtException.GetString().Should().Contain($"Input page limit exceed the max limit of {Common.MAX_PAGE_LIMIT}");
+
+      using TestApplicationEngine engineStep3 = new(snapshot, settings, owner, WitnessScope.Global);
+      engineStep3.ExecuteScript<Legends>(c => c.listNftPool(0, nftQuantity)); // List from page 0
+      engineStep3.State.Should().Be(VMState.FAULT);
+      engineStep3.UncaughtException.GetString().Should().Contain("Pagination data must be provided, pageNumber and pageSize must have at least 1");
+
+      using TestApplicationEngine engineStep4 = new(snapshot, settings, owner, WitnessScope.Global);
+      engineStep4.ExecuteScript<Legends>(c => c.listNftPool(Common.MAX_PAGE_LIMIT, Common.MAX_PAGE_LIMIT)); // List page over the total pages
+      engineStep4.State.Should().Be(VMState.FAULT);
+      engineStep4.UncaughtException.GetString().Should().Contain($"Input page number exceed the totalPages of 1");
     }
   }
 }
