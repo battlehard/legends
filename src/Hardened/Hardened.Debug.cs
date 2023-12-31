7268cabf91383adec55b97a8ca78834a9e9e5fc1
Rawich Kiriyapong <rawich_kiriyapong@hotmail.com>
2023-12-31 10:07:36 +0700

    Update test case and add test suite to make debug method private

--- src/Hardened/Hardened.Debug.cs
+++ src/Hardened/Hardened.Debug.cs
@@ -6,31 +6,37 @@ namespace Hardened
 {
   public partial class Hardened
   {
-    public static void Debug_ManageAdmin()
+    public static void TestSuite()
     {
       CheckContractAuthorization();
+      Debug_ManageAdmin();
+    }
+    private static void Debug_ManageAdmin()
+    {
+
       UInt160 admin1 = ToScriptHash("Nh9XK6sZ6vkPu9N2L9GxnkogxXKVCgDMws");
       UInt160 admin2 = ToScriptHash("NQbHe4RTkzwGD3tVYsTcHEhxWpN4ZEuMuG");
 
       // Case 1: No admin
-      Assert(GetAdmin().Count == 0, "Error: Expected empty list");
+      Assert(GetAdmin().Count == 0, "Expected empty list");
       Runtime.Notify("adminList case 1", new object[] { GetAdmin() });
 
       // Case 2: Add the first admin
       SetAdmin(admin1);
-      Assert(GetAdmin().Count == 1 && GetAdmin()[0] == admin1, "Error: Expected admin1");
+      Assert(GetAdmin().Count == 1 && GetAdmin()[0] == admin1, "Expected admin1");
       Runtime.Notify("adminList case 2", new object[] { GetAdmin() });
 
       // Case 3: Add the second admin
       SetAdmin(admin2);
-      Assert(GetAdmin().Count == 2, "Error: Expected two admin");
-      Assert(AdminHashesStorage.IsExist(admin1), "Error: Expected admin1");
-      Assert(AdminHashesStorage.IsExist(admin2), "Error: Expected admin2");
+      Assert(GetAdmin().Count == 2, "Expected two admin");
+      Assert(AdminHashesStorage.IsExist(admin1), "Expected admin1");
+      Assert(AdminHashesStorage.IsExist(admin2), "Expected admin2");
       Runtime.Notify("adminList case 3", new object[] { GetAdmin() });
 
       // Case 4: Delete the first admin
       DeleteAdmin(admin1);
-      Assert(GetAdmin().Count == 1 && GetAdmin()[0] == admin2, "Error: Expected admin2");
+      Assert(AdminHashesStorage.IsExist(admin1) == false, "Expected no admin1");
+      Assert(GetAdmin().Count == 1 && GetAdmin()[0] == admin2, "Expected admin2");
       Runtime.Notify("adminList case 4", new object[] { GetAdmin() });
     }
   }
